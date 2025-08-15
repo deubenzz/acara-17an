e791cfb6b68aa5295403689b3b18ff0e2e807b1e
deubenzz <106050289+deubenzz@users.noreply.github.com>
2025-08-15 14:50:45 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,37 @@
-    // Nama cache
-    const CACHE_NAME = 'gebyar-kemerdekaan-cache-v2';
-    // Daftar file yang akan di-cache
-    const urlsToCache = [
-      '/'
-    ];
+// Nama cache
+const CACHE_NAME = 'gebyar-kemerdekaan-cache-v2';
+// Daftar file yang akan di-cache
+const urlsToCache = [
+  '/'
+];
 
-    self.addEventListener('install', function(event) {
-      event.waitUntil(
-        caches.open(CACHE_NAME)
-          .then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-          })
-      );
-    });
+self.addEventListener('install', function(event) {
+  // Perintahkan service worker baru untuk aktif segera
+  self.skipWaiting(); 
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-    self.addEventListener('fetch', function(event) {
-      event.respondWith(
-        caches.match(event.request)
-          .then(function(response) {
-            if (response) {
-              return response;
-            }
-            return fetch(event.request);
-          }
-        )
-      );
-    });
-    
+self.addEventListener('activate', event => {
+  // Perintahkan service worker untuk mengambil alih semua halaman yang terbuka
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
